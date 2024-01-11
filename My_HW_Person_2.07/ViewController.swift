47586bb253a85f68b23f1d47ad502fe667a8ccbb
Dmitry Parkhomenko <oku_74@mail.ru>
2024-01-11 20:45:40 +0500

    Initial Commit

---
+++ My_HW_Person_2.07/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My_HW_Person_2.07
+//
+//  Created by Dmitry Parhomenko on 11.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
